913e5b95c188a2829dbd01c078c5a8233bbeb04f
codedotking <huala.fun@outlook.com>
2024-09-11 19:15:11 +0800

    fix

---
+++ src/components/cv-editor/Component.tsx
@@ -0,0 +1,20 @@
+import { NodeViewWrapper } from "@tiptap/react";
+
+export default (props: any) => {
+  const increase = () => {
+    props.updateAttributes({
+      count: props.node.attrs.count + 1,
+    });
+  };
+
+  return (
+    <NodeViewWrapper className="react-component">
+      <label>React Component</label>
+      <div className="content">
+        <button onClick={increase}>
+          This button has been clicked {props.node.attrs.count} times.
+        </button>
+      </div>
+    </NodeViewWrapper>
+  );
+};
